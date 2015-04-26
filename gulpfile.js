f5c3a667c6045ea37525c475dd15fa140cb03a0b
Ashton Harris <ashtonharris.me@gmail.com>
2015-04-26 13:33:02 -0400

    Restructure file structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,33 +9,41 @@ gulp.task('stylus', function () {
     jeet = require('jeet'),
     rupture = require('rupture');
 
-  return gulp.src(['**/*.styl', '!**/_*', '!./styl/lib/**/*.styl', '!./node_modules/**/*.styl'])
+  return gulp.src(['src/**/*.styl', '!**/_*', '!./styl/lib/**/*.styl'])
     .pipe(stylus({use: [nib(), jeet(), rupture()]}))
     .pipe(flatten())
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./dest/css'))
     .pipe(connect.reload());
 });
 
 gulp.task('stylint', function() {
   var stylint = require('gulp-stylint');
-  return gulp.src(['**/*.styl', '!./styl/lib/**/*.styl', '!./node_modules/**/*.styl'])
+  return gulp.src(['src/**/*.styl', '!./src/styl/lib/**/*.styl'])
     .pipe(stylint({config: '.stylintrc'}))
+});
+
+gulp.task('sass', function () {
+  var sass = require('gulp-sass');
+
+  gulp.src(['src/**/*.sass'])
+    .pipe(sass({indentedSyntax: true}))
+    .pipe(gulp.dest('./dest/css'));
 });
 
 gulp.task('less', function () {
   var less = require('gulp-less');
 
-  return gulp.src(['**/*.less', '!**/_*', '!./less/lib/**/*.less', '!./node_modules/**/*.less'])
+  return gulp.src(['src/**/*.less', '!src/**/_*', '!./src/less/lib/**/*.less'])
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dest/css'));
 });
 
 gulp.task('templatizer', function() {
   var templatizer = require('templatizer');
 
-  templatizer('./**/*.jade', './temp/templatizer.js', {
+  templatizer('./src/**/*.jade', './temp/templatizer.js', {
     namespace: 'App',
     dontremoveMixins: true
   });
@@ -45,22 +53,23 @@ gulp.task('jade', function() {
   var jade = require('gulp-jade'),
     gutil = require('gulp-util');
 
-  gulp.src(['./templates/**/*.jade', '!./templates/**/_*.jade'])
+  gulp.src(['./src/templates/**/*.jade', '!./src/templates/**/_*.jade'])
     .pipe(jade())
     .on('error', gutil.log)
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./dest'))
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['**/*.styl'], ['stylus', 'stylint']);
-  gulp.watch(['**/*.jade'], ['jade', 'templatizer']);
-  gulp.watch(['**/*.less'], ['less']);
+  gulp.watch(['src/**/*.styl'], ['stylus', 'stylint']);
+  gulp.watch(['src/**/*.jade'], ['jade', 'templatizer']);
+  gulp.watch(['src/**/*.less'], ['less']);
+  gulp.watch(['src/**/*.sass'], ['sass']);
 });
 
 gulp.task('connect', function() {
   connect.server({
-    root: [__dirname],
+    root: ['bower_components', 'temp', 'src', 'dest'],
     livereload: true
   });
 });
@@ -69,6 +78,7 @@ gulp.task('default', [
   'stylus',
   'stylint',
   'less',
+  'sass',
   'jade',
   'templatizer',
   'connect',
